445598fd5ecf455220b534a25a0c3fb89e03e433
shunya hayashi <hayashun0226@icloud.com>
2023-05-11 00:15:19 +0900

    add doc一覧を取得

--- cli/aozora-collector/main.go
+++ cli/aozora-collector/main.go
@@ -1,7 +1,45 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+
+	"github.com/PuerkitoBio/goquery"
+)
+
+type Entry struct {
+	AuthorID string
+	Author string
+	TitleID string
+	Title string
+	InfoURL string
+	ZipURL string
+}
+
+func findEntries(siteURL string) ([]Entry, error) {
+  doc, err := goquery.NewDocument(siteURL)
+  if err != nil {
+	return nil, err
+  } 
+
+  doc.Find("ol li a").Each(func(n int, elem *goquery.Selection) {
+    println(elem.Text(), elem.AttrOr("href", ""))
+  })
+
+return nil, nil
+}
+
 
 func main() {
-	fmt.Println("Hello, This is aozora collector cli!")
+  listURL := "https://www.aozora.gr.jp/index_pages/person879.html"
+
+  entries, err := findEntries(listURL)
+
+  if err != nil {
+	log.Fatal(err)
+  }
+
+  for _, entry := range entries {
+	fmt.Println(entry.Title, entry.ZipURL)
+  }
 }

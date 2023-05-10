bb5a6c55bfa8e6a6b05d00255a7f1e97d8bc0caf
shunya hayashi <hayashun0226@icloud.com>
2023-05-11 03:04:13 +0900

    add zipファイルのpathを取得

--- cli/aozora-collector/main.go
+++ cli/aozora-collector/main.go
@@ -3,6 +3,10 @@ package main
 import (
 	"fmt"
 	"log"
+	"net/url"
+	"path"
+	"regexp"
+	"strings"
 
 	"github.com/PuerkitoBio/goquery"
 )
@@ -18,15 +22,67 @@ type Entry struct {
 
 func findEntries(siteURL string) ([]Entry, error) {
   doc, err := goquery.NewDocument(siteURL)
+
   if err != nil {
 	return nil, err
   } 
 
+  pat := regexp.MustCompile(`.*/cards/([0-9]+)/card([0-9]+).html$`)
+
   doc.Find("ol li a").Each(func(n int, elem *goquery.Selection) {
-    println(elem.Text(), elem.AttrOr("href", ""))
+
+	token := pat.FindStringSubmatch(elem.AttrOr("href", ""))
+
+	if len(token) != 3 {
+		return
+	}
+
+  pageURL := fmt.Sprintf("https://www.aozora.gr.jp/cards/%s/card%s.html", token[1], token[2])
+
+  _, zipURL := findAuthorAndZip(pageURL)
+
+  println(zipURL)
+
   })
 
 return nil, nil
+}
+
+func findAuthorAndZip(siteURL string) (string, string) {
+	doc, err := goquery.NewDocument(siteURL)
+
+	if err != nil {
+		return "", ""
+	}
+
+	author := doc.Find("table[summary=作家データ] tr:nth-child(1) td:nth-child(2)").Text()
+
+	zipURL := ""
+
+	doc.Find("table.download a").Each(func(n int, elem *goquery.Selection) {
+		href := elem.AttrOr("href", "")
+		if strings.HasSuffix(href, ".zip") {
+			zipURL = href
+		}
+	})
+
+	if zipURL == "" {
+		return author, ""
+	}
+
+	if strings.HasPrefix(zipURL, "http://") || strings.HasPrefix(zipURL, "https://") {
+		return author, zipURL
+	}
+
+	u, err := url.Parse(siteURL)
+	
+	if err != nil {
+		return author, ""
+	}
+
+	u.Path = path.Join(path.Dir(u.Path), zipURL)
+
+	return author, u.String()
 }
 
 

6b6c42d70679b3f9f30295631d6bd31880d7e74b
shunya hayashi <hayashun0226@icloud.com>
2023-05-27 05:43:31 +0000

    add aozora-search

--- cli/cmd/aozora-search/main.go
+++ cli/cmd/aozora-search/main.go
@@ -6,19 +6,106 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strings"
+
+	"github.com/ikawaha/kagome-dict/ipa"
+	"github.com/ikawaha/kagome/v2/tokenizer"
 )
 
 func showAuthors(db *sql.DB) error {
+
+	rows, err := db.Query(`
+	SELECT 
+		a.author_id, 
+		a.author
+	FROM 
+	 	autors a
+	ORDER BY 
+		CAST(a.author_id AS INTEGER)
+	`)
+
+	if err != nil {
+		return err
+	}
+
+	defer rows.Close()
+
+	for rows.Next() {
+		var authorID, author string
+		err = rows.Scan(&authorID, &author)
+		if err != nil {
+			return err
+		}
+		fmt.Printf("%s %s\n", authorID, author)
+	}
 
 	return nil
 }
 
 func showContent(db *sql.DB, authorId string, titleId string) error {
+	var content string
+
+	err := db.QueryRow(`
+	SELECT 
+	 	c.content
+	FROM 
+		content c
+	WHERE
+		c.author_id = ?
+	AND c.title_id = ?
+	`, authorId, titleId).Scan(&content)
+
+	if err != nil {
+		return err
+	}
+
+	fmt.Println(content)
 
 	return nil
 }
 
 func queryContent(db *sql.DB, query string) error {
+	t, err := tokenizer.New(ipa.Dict(), tokenizer.OmitBosEos())
+
+	if err != nil {
+		return err
+	}
+
+	seg := t.Wakati(query)
+
+	rows, err := db.Query(`
+	SELECT
+		a.author_id, 
+		a.author, 
+		c.title_id, 
+		c.title
+	FROM 
+	    contents c
+	INNER JOIN authors a
+		ON a.author_id = c.author_id
+	INNER JOIN contents_fts f
+		ON c.rowid = f.docid
+		AND words MATCH ?
+	`, strings.Join(seg, " "))
+
+	if err != nil {
+		return err
+	}
+
+	defer rows.Close()
+
+	for rows.Next() {
+		var authorID, author string
+		var titleID, title string
+
+		err = rows.Scan(&authorID, &author, &titleID, &title)
+
+		if err != nil {
+			return err
+		}
+
+		fmt.Printf("%s % 5s: %s (%s)\n", authorID, titleID, title, author)
+	}
 
 	return nil
 }
@@ -69,5 +156,4 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-
 }
